444c8a717edbc719c623169da5d4903a3df4fc4c
Pratim Sarkar <pratimsarkar003@gmail.com>
2021-08-22 01:10:55 +0530

    Updating android

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
     public static void main(String[] args){
-        System.out.println("Hello S");
+        System.out.println("Hello Android S");
         System.out.println("Android S is coming");
     }
 }
