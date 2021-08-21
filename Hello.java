b022601b760602ff06997249909675c2928df466
Pratim Sarkar <pratimsarkar@Pratims-MacBook-Air.local>
2021-08-22 01:12:08 +0530

    Hello S first  printed

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
     public static void main(String[] args){
-        System.out.println("Hello S");
+        System.out.println("Hello S Version first time");
         System.out.println("Android S is coming");
     }
 }
