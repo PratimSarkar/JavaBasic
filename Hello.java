88b40ae9e44278dc3cc34d590c95a189bb48186d
Pratim Sarkar <pratimsarkar@Pratims-MacBook-Air.local>
2021-08-22 01:15:27 +0530

    Hello T  printed

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
     public static void main(String[] args){
-        System.out.println("Hello Android S");
+        System.out.println("Hello Android T");
         System.out.println("Android S is coming");
     }
 }
